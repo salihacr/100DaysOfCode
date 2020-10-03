005ca286ccf8ff0fe87bea81d201ef3742e4ec32
Salih ACUR <35336217+salihacr@users.noreply.github.com>
2020-10-03 17:00:30 +0300

    Update Day1.cs

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -53,7 +53,7 @@ class HelloWorld {
         C(power, i)   = power! / ( (power-i)! * i! ) 
     */ 
     static double C(double n, double i){
-        return factorial(n)/(factorial(i)*factorial(n-i));
+        return factorial(n)/(factorial(n-i)*factorial(i));
     }
     /*
         Binom Expansion Formula2 for Each Item
