f53de38ba6122823356eca989ab7194cc6193c5d
salihacr <salihacur1@gmail.com>
2021-03-16 22:35:17 +0300

    builder and iterator pattern

---
+++ P9-Day88/DesignPatterns.Builder/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace DesignPatterns.Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            YemekBuilder builder = new SuluYemekConcreteBuilder();
+            YemekYapici yemekYapici = new YemekYapici();
+            yemekYapici.YemekYap(builder);
+            builder.Yemek.ToString();
+
+            builder = new EtliYemekConcreteBuilder();
+            yemekYapici.YemekYap(builder);
+            builder.Yemek.ToString();
+
+            Console.ReadKey();
+        }
+    }
+    enum YemekTipi
+    {
+        Sulu,
+        Etli,
+        Sebzeli
+    }
+    // Product
+    class Yemek
+    {
+        public YemekTipi YemekTipi { get; set; }
+        public string YemekAdi { get; set; }
+        public double Tuz { get; set; }
+        public override string ToString()
+        {
+            Console.WriteLine($"{YemekTipi} -> Tuz Oranı : {Tuz}");
+            return base.ToString();
+        }
+    }
+    //Builder Class
+    abstract class YemekBuilder
+    {
+        protected Yemek yemek;
+
+        public Yemek Yemek
+        {
+            get
+            {
+                return yemek;
+            }
+        }
+        abstract public void SetYemekTipi();
+        abstract public void SetYemekAdi();
+        abstract public void SetTuz();
+    }
+    // Concrete Builder Classes
+    class SuluYemekConcreteBuilder : YemekBuilder
+    {
+        public SuluYemekConcreteBuilder()
+        {
+            yemek = new Yemek();
+        }
+        public override void SetTuz() => yemek.Tuz = 75;
+
+        public override void SetYemekAdi() => yemek.YemekAdi = "Çorba";
+
+        public override void SetYemekTipi() => yemek.YemekTipi = YemekTipi.Sulu;
+    }
+    class EtliYemekConcreteBuilder : YemekBuilder
+    {
+        public EtliYemekConcreteBuilder()
+        {
+            yemek = new Yemek();
+        }
+        public override void SetTuz() => yemek.Tuz = 65;
+
+        public override void SetYemekAdi() => yemek.YemekAdi = "Etli Pilav";
+
+        public override void SetYemekTipi() => yemek.YemekTipi = YemekTipi.Etli;
+    }
+    class SebzeliYemekConcreteBuilder : YemekBuilder
+    {
+        public SebzeliYemekConcreteBuilder()
+        {
+            yemek = new Yemek();
+        }
+        public override void SetTuz() => yemek.Tuz = 15;
+
+        public override void SetYemekAdi() => yemek.YemekAdi = "Ispanak";
+
+        public override void SetYemekTipi() => yemek.YemekTipi = YemekTipi.Sebzeli;
+    }
+
+    // Director class
+    class YemekYapici
+    {
+        public void YemekYap(YemekBuilder builder)
+        {
+            builder.SetTuz();
+            builder.SetYemekAdi();
+            builder.SetYemekTipi();
+        }
+    }
+}
---
+++ P9-Day88/DesignPatterns.Iterator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace DesignPatterns.Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTimeAggregate date = new DateTimeAggregate();
+            date.startDate = new DateTime(2021, 01, 01);
+            date.endDate = DateTime.Now;
+            IIterator iterator = date.CreateIterator();
+            while (iterator.HasDate())
+            {
+                Console.WriteLine(iterator.CurrentDate());
+            }
+            Console.ReadKey();
+        }
+    }
+    interface IAggregate
+    {
+        IIterator CreateIterator();
+    }
+    interface IIterator
+    {
+        bool HasDate();
+        DateTime CurrentDate();
+    }
+    class DateTimeAggregate : IAggregate
+    {
+        public DateTime startDate;
+        public DateTime endDate;
+        public IIterator CreateIterator() => new DateTimeIterator(this);
+    }
+    class DateTimeIterator : IIterator
+    {
+        DateTimeAggregate aggregate;
+        DateTime currentDate;
+        public DateTimeIterator(DateTimeAggregate aggregate)
+        {
+            this.aggregate = aggregate;
+            currentDate = aggregate.startDate;
+        }
+        public DateTime CurrentDate() => currentDate;
+
+        public bool HasDate()
+        {
+            if (currentDate.DayOfWeek == DayOfWeek.Saturday || currentDate.DayOfWeek == DayOfWeek.Sunday)
+            {
+                int dayCount = currentDate.DayOfWeek == DayOfWeek.Saturday ? 1 : 6;
+                currentDate = currentDate.AddDays(dayCount);
+            }
+            else
+            {
+                int dayCount = (int)currentDate.DayOfWeek;
+                currentDate = currentDate.AddDays(6 - dayCount);
+            }
+            return currentDate < aggregate.endDate;
+        }
+    }
+}
